5f449b6856d7a0bfac9cdb9b3fa5d0c4ac2e0340
Christoph Schlüter <chrisschlueter@t-online.de>
2023-10-18 21:43:30 +0200

    feat: added summary description copy button

--- WorkTimeClock/Views/Days/Summary/DaySummaryEntryView.swift
+++ WorkTimeClock/Views/Days/Summary/DaySummaryEntryView.swift
@@ -11,6 +11,8 @@ struct DaySummaryEntryView: View {
     @ObservedObject var tagComponent = ComponentManager.instance.getComponent(TagComponent.self)!
     let entry: DaySummaryEntryObject
     
+    @State var hoveringOverDescription = false
+    
     var body: some View {
         VStack(spacing: 8) {
             HStack {
@@ -26,17 +28,39 @@ struct DaySummaryEntryView: View {
             
             Divider()
             
-            Text(entry.description.isEmpty ? "Keine Beschreibung vorhanden" : entry.description)
-                .font(.system(.body))
-                .italic()
-                .multilineTextAlignment(.center)
-                .onTapGesture {
-                    if !entry.description.isEmpty {
-                        let pasteboard = NSPasteboard.general
-                        pasteboard.declareTypes([.string], owner: nil)
-                        pasteboard.setString(entry.description, forType: .string)
+            ZStack {
+                Text(entry.description.isEmpty ? "Keine Beschreibung vorhanden" : entry.description)
+                    .font(.system(.body))
+                    .italic()
+                    .multilineTextAlignment(.center)
+                    .frame(minHeight: 36)
+                
+                HStack(spacing: 0) {
+                    Spacer()
+                    VStack(spacing: 0) {
+                        if !entry.description.isEmpty && hoveringOverDescription {
+                            Button {
+                                let pasteboard = NSPasteboard.general
+                                pasteboard.declareTypes([.string], owner: nil)
+                                pasteboard.setString(entry.description, forType: .string)
+                            } label: {
+                                Image(systemName: "doc.on.doc")
+                            }
+                            .buttonStyle(.plain)
+                            .padding(.horizontal, 8)
+                            .padding(.vertical, 4)
+                            .background(.background)
+                            .clipShape(Capsule(style: .continuous))
+                        }
+                        Spacer()
                     }
                 }
+            }
+            .onHover { hovering in
+                withAnimation {
+                    hoveringOverDescription = hovering
+                }
+            }
         }
         .padding(12)
         .background()
